184311a99009b142e611caa0841a020770e25b08
Yaroslav Halchenko <debian@onerussian.com>
2009-02-12 23:34:28 -0500

    RF: trailing whitespace + untabify (was a mix of spaces + tabs)

--- pythm/gtkgui/pageplay.py
+++ pythm/gtkgui/pageplay.py
@@ -19,8 +19,8 @@ from gtkhelper import ImageButton
 
 class PagePlay(Page):
     def __init__(self):
-	self.duration = 0.0	  # Druation of current song.
-	self.doPosUpdates = True  # Set song position from backend in slider.
+        self.duration = 0.0       # Druation of current song.
+        self.doPosUpdates = True  # Set song position from backend in slider.
 
         Page.__init__(self);
         self.cfg.get_backend().connect(Signals.VOLUME_CHANGED,self.volume_changed)
@@ -29,10 +29,10 @@ class PagePlay(Page):
         self.cfg.get_backend().connect(Signals.SONG_CHANGED,self.song_changed)
         self.cfg.get_backend().connect(Signals.POS_CHANGED,self.pos_changed)
         self.cfg.get_backend().connect(Signals.STATE_CHANGED,self.state_changed)
-        
+
         self.btn_prev = ImageButton(gtk.STOCK_MEDIA_PREVIOUS)
         self.btnbox.add(self.btn_prev)
-	self.btn_stop = ImageButton(gtk.STOCK_MEDIA_STOP)
+        self.btn_stop = ImageButton(gtk.STOCK_MEDIA_STOP)
         self.btnbox.add(self.btn_stop)
         self.btn_pause = ImageButton(gtk.STOCK_MEDIA_PAUSE)
         self.btnbox.add(self.btn_pause)
@@ -40,17 +40,17 @@ class PagePlay(Page):
         self.btnbox.add(self.btn_play)
         self.btn_next = ImageButton(gtk.STOCK_MEDIA_NEXT)
         self.btnbox.add(self.btn_next)
-        
-        
+
+
         self.btn_play.connect("clicked", self.btn_clicked)
         self.btn_stop.connect("clicked", self.btn_clicked)
         self.btn_prev.connect("clicked", self.btn_clicked)
         self.btn_next.connect("clicked", self.btn_clicked)
         self.btn_pause.connect("clicked", self.btn_clicked)
-        
+
         self.volevent = False
- 
-    def btn_clicked(self,widget):        
+
+    def btn_clicked(self,widget):
         if widget == self.btn_next:
             self.cfg.get_backend().next()
         elif widget == self.btn_play:
@@ -62,7 +62,7 @@ class PagePlay(Page):
         elif widget == self.btn_prev:
             self.cfg.get_backend().prev()
 
-        
+
     def state_changed(self,newstate):
         if newstate == State.STOPPED:
             self.btn_stop.set_sensitive(False)
@@ -79,26 +79,26 @@ class PagePlay(Page):
 
     def song_changed(self,newplentry):
         self.songlabel.set_label(str(newplentry))
-	
+
         if newplentry.length > 0:
             self.pos_scale.set_range(0,newplentry.length)
-	
-	self.duration = newplentry.length
+
+        self.duration = newplentry.length
         self.pos_changed(0)
 
     def pos_changed(self,newPos):
         self.posevent = True
         newPos = int(newPos)
-	
-	# Set the new value in the controls.
-	if (self.doPosUpdates):
+
+        # Set the new value in the controls.
+        if (self.doPosUpdates):
             self.pos_scale.set_value(newPos)
-	self.set_times_label(newPos)
+        self.set_times_label(newPos)
         self.posevent = False
 
     def random_changed(self,newRand):
         self.random.set_active(newRand)
-        
+
     def repeat_changed(self,newRept):
         self.repeat.set_active(newRept)
 
@@ -112,9 +112,9 @@ class PagePlay(Page):
     " total duration.
     """
     def set_times_label(self, position):
-	lbl = format_time(position) + " / " + format_time(self.duration)
-	self.timelabel.set_label(lbl)
-    
+        lbl = format_time(position) + " / " + format_time(self.duration)
+        self.timelabel.set_label(lbl)
+
     def content(self):
         #track info
         vbox = gtk.VBox()
@@ -122,16 +122,16 @@ class PagePlay(Page):
         vbox.pack_start(self.songlabel,True,True,0)
         self.timelabel = gtk.Label("0:00 / 0:00")
         vbox.pack_start(self.timelabel,True,True,0)
-        
+
         #pos slider
         hbox2 = gtk.HBox()
-	hbox2.set_border_width(20)
+        hbox2.set_border_width(20)
         #hbox2.pack_start(gtk.Label("Pos."),False,False,0)
         self.pos_scale = gtk.HScale();
         self.pos_scale.set_property("draw-value",False)
-	self.pos_scale.set_value_pos(1)
-	self.pos_scale.connect("button-press-event", self.on_pos_press)
-	self.pos_scale.connect("button-release-event", self.on_pos_release)
+        self.pos_scale.set_value_pos(1)
+        self.pos_scale.connect("button-press-event", self.on_pos_press)
+        self.pos_scale.connect("button-release-event", self.on_pos_release)
         self.pos_scale.connect("value-changed",self.on_pos_change)
         self.pos_scale.set_increments(5,10)
         self.pos_scale.set_update_policy(gtk.UPDATE_DISCONTINUOUS)
@@ -140,19 +140,19 @@ class PagePlay(Page):
 
         #volume,random,repeat
         hbox = gtk.HBox()
-	hbox.set_border_width(10)
+        hbox.set_border_width(10)
         hbox.pack_start(gtk.Label("Vol."),False,False,0)
         self.vol_scale = gtk.HScale();
         self.vol_scale.set_range(50,100)
         self.vol_scale.set_property("draw-value",True)
-	self.vol_scale.set_digits(0)
-	self.vol_scale.set_value_pos(1)
+        self.vol_scale.set_digits(0)
+        self.vol_scale.set_value_pos(1)
         self.vol_scale.connect("value-changed",self.on_volume_change)
         self.vol_scale.set_increments(1,2)
         self.vol_scale.set_update_policy(gtk.UPDATE_DISCONTINUOUS)
         hbox.add(self.vol_scale)
         vbox.add(hbox)
-                
+
         #random, repeat
         hbox3 = gtk.HBox()
         self.random = gtk.CheckButton(label="random")
@@ -161,21 +161,21 @@ class PagePlay(Page):
         hbox3.pack_start(self.repeat,True,False,0)
         self.random.connect("toggled",self.on_random)
         self.repeat.connect("toggled",self.on_repeat)
-        
+
         vbox.add(hbox3)
-        
+
         return vbox
-    
+
     def on_random(self,widget):
         self.cfg.get_backend().set_random(widget.get_active())
-        
+
     def on_repeat(self,widget):
         self.cfg.get_backend().set_repeat(widget.get_active())
-    
+
     def on_volume_change(self,range):
         if self.volevent == False:
             self.cfg.get_backend().set_volume(range.get_value())
-            
+
 
     """
     " Event when the position slider is touched.
@@ -183,16 +183,15 @@ class PagePlay(Page):
     " Thise makes using the control easier.
     """
     def on_pos_press(self, widget, event):
-	self.doPosUpdates = False
-    
+        self.doPosUpdates = False
+
     """
     " Event when the position slider is released.
     " Release position update ignore.
     """
     def on_pos_release(self, widget, event):
-	self.doPosUpdates = True
+        self.doPosUpdates = True
 
     def on_pos_change(self,range):
         if self.posevent == False:
             self.cfg.get_backend().seek(range.get_value())
-        
