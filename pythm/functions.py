ede0f0be0e1064c7c27947d22aca39c3db5c7915
Yaroslav Halchenko <debian@onerussian.com>
2009-02-12 23:27:55 -0500

    BIG RF: functions.py -> untabify + trailing spaces removal
    there was  a mix of indents with spaces and tabs

--- pythm/functions.py
+++ pythm/functions.py
@@ -13,26 +13,26 @@ Helper functions for pythm
 """
 
 import os,re
-from mutagen.easyid3 import EasyID3                                            
-from mutagen.mp3 import MP3                                                    
-from mutagen.oggvorbis import OggVorbis                                        
+from mutagen.easyid3 import EasyID3
+from mutagen.mp3 import MP3
+from mutagen.oggvorbis import OggVorbis
 from mutagen.flac import FLAC
 
-# Audio file artist tag name.                                                  
-TAG_NAME_ARTIST = "artist"                                                     
-# Audion file album tag name.                                                  
-TAG_NAME_TITLE = "title"                                                       
-# File extension for mp3 files.                                                
-FILE_EXT_MP3 = ".mp3"                                                          
-# File extension for ogg files.                                                
-FILE_EXT_OGG = ".ogg"                                                          
-# File extension for flac files.                                               
+# Audio file artist tag name.
+TAG_NAME_ARTIST = "artist"
+# Audion file album tag name.
+TAG_NAME_TITLE = "title"
+# File extension for mp3 files.
+FILE_EXT_MP3 = ".mp3"
+# File extension for ogg files.
+FILE_EXT_OGG = ".ogg"
+# File extension for flac files.
 FILE_EXT_FLAC = ".flac"
 
 # List of regexes for get_tags_from_file
-_filematchers = []                                             
-_filematchers.append(".*-(?P<artist>.*)-(?P<title>.*)\..*")    
-_filematchers.append("(?P<artist>.*)-(?P<title>.*)\..*")       
+_filematchers = []
+_filematchers.append(".*-(?P<artist>.*)-(?P<title>.*)\..*")
+_filematchers.append("(?P<artist>.*)-(?P<title>.*)\..*")
 _filematchers.append("(?P<title>.*)\..*")
 
 """
@@ -60,19 +60,19 @@ def format_time(value):
         sec = "0" + str(sec)
     return str(min) + ":" + str(sec)
 
-"""                                    
+"""
 " returns (artist,title) from regex filename match
 " param file File name to parse.
-"""                                
+"""
 def get_tags_from_file(file):
     for r in _filematchers:
         m = re.match(r,os.path.basename(file))
-        if(m):     
-            try:                          
+        if(m):
+            try:
                 art = m.group("artist").strip()
             except:
                 art = "unknown"
-            try:    
+            try:
                 tit = m.group("title").strip()
             except:
                 tit = "unknown"
@@ -85,21 +85,21 @@ def get_tags_from_file(file):
 " param logger Reference to logger. None if no logger.
 """
 def read_audio_tags(entry, logger):
-	try:                  
-	    fn    = entry.id
-            ext   = os.path.splitext(fn)[1].lower()               
-            audio = None                                   
-                                           
-            if   (ext == FILE_EXT_MP3):  audio = MP3(fn, ID3=EasyID3)
-            elif (ext == FILE_EXT_OGG):  audio = OggVorbis(fn)
-            elif (ext == FILE_EXT_FLAC): audio = FLAC(fn)
-                                          
-            if (audio != None):
-                entry.artist = audio[TAG_NAME_ARTIST][0]  
-                entry.title  = audio[TAG_NAME_TITLE][0]
-                entry.length = audio.info.length
-                                                               
-        except Exception, e:         
-	    if (logger != None):
-            	logger.warn("Failed to read audio tag data: %s" % e)
+    try:
+        fn    = entry.id
+        ext   = os.path.splitext(fn)[1].lower()
+        audio = None
+
+        if   (ext == FILE_EXT_MP3):  audio = MP3(fn, ID3=EasyID3)
+        elif (ext == FILE_EXT_OGG):  audio = OggVorbis(fn)
+        elif (ext == FILE_EXT_FLAC): audio = FLAC(fn)
+
+        if (audio != None):
+            entry.artist = audio[TAG_NAME_ARTIST][0]
+            entry.title  = audio[TAG_NAME_TITLE][0]
+            entry.length = audio.info.length
+
+    except Exception, e:
+        if (logger != None):
+                logger.warn("Failed to read audio tag data: %s" % e)
 
