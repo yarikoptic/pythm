d4ed698cff05839d0743328faebff0fa148f788f
Yaroslav Halchenko <debian@onerussian.com>
2009-02-12 22:53:32 -0500

    NF: added a common header to all files with modelines and reference to COPYING
    To ease collaboration by multiple people with different tastes, it is
    better to prevent them reformatting the code with their default settings
    (such as indents with tabs)
    
    Now we got vim and emacs modelines as well as the reference to future
    COPYING file where to list the copyright holders and the license
    (GPL-2+)

--- pythm/functions.py
+++ pythm/functions.py
@@ -1,3 +1,13 @@
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+#
+#   See COPYING file distributed along with the pythm for the
+#   copyright and license terms.
+#
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+""" TODO: Module description """
+
 """
 Helper functions for pythm
 """
