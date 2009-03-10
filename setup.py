d40e38a2ef7b52baf0a1db8ef51d83d26a227ff0
ptt <code@paultt.org>
2009-03-10 20:27:53 +0100

    updated to -ptt7 version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 import glob
     
 setup(name='Pythm',
-        version='0.5.1',
+        version='0.5.1-ptt7',
         description='Frontend for mplayer and mpd',
         author='Matthias Hans',
         author_email='ughl0r@yahoo.de',
