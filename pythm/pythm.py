baced7140b0c1f1358f8fcb3a83ad0b77acb07ad
Yaroslav Halchenko <debian@onerussian.com>
2009-03-14 12:30:48 -0400

    RF: renicing now is specified as pythm.renice in config
    also
    - deleted trailing spaces in the sample config file
    - default renice to -5 in config
    - use plain .getint guarded in try: except to be more fool-proof

--- pythm/pythm.py
+++ pythm/pythm.py
@@ -6,13 +6,29 @@
 #   copyright and license terms.
 #
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
-""" TODO: Module description """
+""" Starts Pythm's GUI """
 
+import os
 import gtkgui
 import backend
-from config import PythmConfig
+from config import PythmConfig, logger
 
 def startPythm():
+    """Start the Pythm and renice if it was requested
+    """
+    config = PythmConfig()
+    try:
+        renice_level = config.getint("pythm", "renice")
+    except Exception, e:                # could be ValueError, TypeError
+        renice_level = -5
+        logger.warn("Assuming default renice=%d since failed to read "
+                    "pythm.renice configuration: %s" % (renice_level, str(e)))
+    if renice_level != 0:
+        logger.debug("Renicing pythm to %d" % renice_level)
+        try:
+            os.nice(renice_level)
+        except OSError, e:
+            logger.error("Failed to renice: %s" % e)
     gtkgui.PythmGtk()
 
 if __name__ == "__main__":
